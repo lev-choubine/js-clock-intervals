eba612a8cd29d09e6bea6cce78a07f64f9a398de
Lev Michael <lev.choubine@gmail.com>
2020-10-23 15:12:14 -0400

    [js]helps you set the time

--- clock.js
+++ clock.js
@@ -1,9 +1,24 @@
-//Automation for the clock!!!!!!
+//Reseting setting
 let countDown = 0;
 const secondsHand = document.getElementById('second');
-const minutesHand = document.getElementById('minute')
+const minutesHand = document.getElementById('minute');
 
-const hoursHand = document.getElementById('hour')
+const hoursHand = document.getElementById('hour');
+
+
+//Automation for the clock!!!!!!
+function setTime(secs,mins,hours){
+    secPosition = `rotate(${secs * 6}deg)`;
+    minPosition = `rotate(${mins * 6}deg)`;
+    hourPosition = `rotate(${hours * 30}deg)`;
+    hoursHand.style.transform=hourPosition;
+    minutesHand.style.transform=minPosition;
+    secondsHand.style.transform=secPosition;    
+}
+
+setTime(0,9,3);
+
+
 
 function tickSecond() {
 countDown = countDown  + 6;    
@@ -16,10 +31,12 @@ return countDown, countDown2;
     
 }
 
+
+
 function tickMinutes() {
     countDown = countDown  + 6;    
     countDown2 =  `rotate(${countDown}deg)`;
-    minutesHand.style.transform=countDown2;
+    c=countDown2;
     
     console.log(countDown2);
     return countDown, countDown2;
@@ -49,7 +66,7 @@ let seconds = setInterval(tickSecond, 1000);
 // 	clearInterval(seconds);
 // }
 let minutes = setInterval(tickMinutes, 60000);
-let hours = setInterval(tickMinutes, 720000);
+let hours = setInterval(tickMinutes, 36000000);
 //setTimeout(hush, 60000);
 
 // const secondsHand = document.getElementById('second')
